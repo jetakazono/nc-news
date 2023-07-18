b4cd733327899ced4f260350d86b43cf003ab00b
jetakazono <jessica.takazono@gmail.com>
2023-07-18 16:05:19 +0100

    fix like button

--- src/components/CommentsListItem.jsx
+++ src/components/CommentsListItem.jsx
@@ -12,10 +12,13 @@ export const CommentsListItem = ({ comment }) => {
             <p>{formatDate(comment.created_at)}</p>
             <p class="text-gray-600 mt-2">{comment.body}</p>
             <div className="flex justify-end p-4">
-            <svg className="w-6 h-6 text-red-300 hover:text-red-500 cursor-pointer mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 21 19">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M11 4C5.5-1.5-1.5 5.5 4 11l7 7 7-7c5.458-5.458-1.542-12.458-7-7Z"/>
-                        </svg>
-            <span>{comment.votes}</span>
+        <button class="flex items-center ml-6">
+          <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 0h1v3l3 7v8a2 2 0 0 1-2 2H5c-1.1 0-2.31-.84-2.7-1.88L0 12v-2a2 2 0 0 1 2-2h7V2a2 2 0 0 1 2-2zm6 10h3v10h-3V10z"/></svg>
+        </button>
+        <span class="ml-2">{comment.votes}</span>
+        <button class="flex items-center ml-4">
+          <svg class="w-3 h-3" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M11 20a2 2 0 0 1-2-2v-6H2a2 2 0 0 1-2-2V8l2.3-6.12A3.11 3.11 0 0 1 5 0h8a2 2 0 0 1 2 2v8l-3 7v3h-1zm6-10V0h3v10h-3z"/></svg>
+        </button>
             </div>
         </li>
     )
